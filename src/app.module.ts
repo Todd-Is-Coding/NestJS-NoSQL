0d6fb3b8e924d96cfa1d89b9345d2dbb28dfeb99
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-07-11 18:48:46 +0300

    Setting up : setting MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      isGlobal: true,
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
       }),
     }),
-    UsersModule,
+    DatabaseModule,
   ],
   providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
